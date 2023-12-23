dd420470025dd001b27a80ae500baf4da423dc07
Soumyendu <cyberrrnaut@gmail.com>
2023-12-23 23:57:50 +0530

    chore - configuring redux toolkit

---
+++ src/store/store.js
@@ -0,0 +1,12 @@
+import { ConfigureStoreOptions, configureStore } from "@reduxjs/toolkit";
+
+import { combineReducers } from "@reduxjs/toolkit";
+
+const store = configureStore({
+    reducer: {
+        
+    }
+});
+
+
+export default store;
